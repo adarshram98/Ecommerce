a9d9dd01c6e401af50ca8f4a6f429782c2ef86e5
Adarsh Ram <adarshram333@gmail.com>
2020-11-12 12:30:54 +0530

    userSchema encryption and authentication

--- models/user.js
+++ models/user.js
@@ -1,57 +1,71 @@
 var mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
 
+var userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 33,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    maxlength: 33,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  userinfo: {
+    type: String,
+    trim: true,
+  },
+  encrypted_password: {
+    type: String,
+    required: true,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0,
+  },
+  purchases: {
+    type: Array,
+    default: 0,
+  },
+});
 
+userSchema.virtual("password")
+    .set(function(password){
+        this._pass=password
+        this.salt = uuidv4();
+        this.encrypted_password = this.encryptPassword(password)
+    })
+    .get(function(){
+        this._pass
+    })
 
-  var userSchema = new mongoose.Schema({
-    
-    name:{
-        type:String,
-        required:true,
-        maxlength:33,
-        trim:true
+userSchema.method = {
+
+    authenticate: function(plainpassword)
+    {
+        return  this.encryptPassword(plainpassword) === this.encrypted_password
     },
-    lastName:{
-        type :String,
-        maxlength:33,
-        trim:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        trim:true
-    },
-    userinfo:{
-        type:String,
-        trim:true
-    },
-    encrypted_password:{
-        type : String,
-        required:true
-    },
-    salt:String,
-    role:{
-        type:Number,
-        default:0
-    },
-    purchases:{
-        type:Array,
-        default:0
+  encryptPassword: function (passwword) {
+    if (!passwword) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(passwword)
+        .digest("hex");
+    } catch (error) {
+      return "";
     }
-    
-  });
-
-  userSchema.method={
-      encryptPassword:function(passwword){
-          if(!passwword) return ""
-          try {
-            return crypto.createHmac('sha256', this.salt)
-            .update(passwword)
-            .digest('hex');
-          } catch (error) {
-              return ""
-          }
-
-      }
-  }
-  module.exports = mongoose.model("User",userSchema)
+  },
+};
+module.exports = mongoose.model("User", userSchema);

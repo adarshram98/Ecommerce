ca08b0beb87c3ac82b0e934c27a5d26b367017cf
Adarsh Ram <adarshram333@gmail.com>
2020-11-13 12:15:36 +0530

    Sign up Valiation done

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,6 @@
+const User = require("../models/user")
+const { check, validationResult } = require('express-validator');
+
 exports.signOut =  (req, res) => {
     //console.log("wew")
       res.send("Sign out successful");
@@ -5,5 +8,27 @@ exports.signOut =  (req, res) => {
 
     exports.signUp =  (req, res) => {
         //console.log("wew")
-          res.send("Sign up successful");
+          const user = new User(req.body)
+
+        //check for validation errors and throw them
+        const errors = validationResult(req)
+          if(!errors.isEmpty())
+          {
+            return res.status(422).json({
+              error: errors.array()[0].msg 
+            })
+          }
+          //saving user to database
+          user.save((err,user)=>{
+              if(err)
+              {
+                  return res.status(400).json(
+                      {
+                        err:"Not able to add user in DB"  
+                      }
+                  )
+                  
+              }
+              res.json(user)
+          })
         }
--- routes/authentication.js
+++ routes/authentication.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
-
+const { check,validationResult} = require('express-validator');
+ 
 var express = require("express");
 var router = express.Router();
 
@@ -7,6 +8,12 @@ const {signOut,signUp} = require("../controllers/authentication")
 
 router.get("/signout",signOut);
 
-module.exports = router;
 
-router.post("/signup",signUp)
+
+router.post("/signup",[
+    check("name").isLength({min:2}).withMessage("Name should be atleast 2 charecter"),
+    check("email").isEmail().withMessage("email is required"),
+    check("password").isLength({min:5}).withMessage("Min lenght 5")
+],signUp)
+
+module.exports = router;

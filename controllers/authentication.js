29915a4cf94397bdab866b54559de56f9ca16fda
Adarsh Ram <adarshram333@gmail.com>
2020-11-13 14:51:16 +0530

    login route created

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,34 +1,67 @@
-const User = require("../models/user")
-const { check, validationResult } = require('express-validator');
+const User = require("../models/user");
+const { check, validationResult } = require("express-validator");
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
-exports.signOut =  (req, res) => {
-    //console.log("wew")
-      res.send("Sign out successful");
+exports.signUp = (req, res) => {
+  //console.log("wew")
+  const user = new User(req.body);
+
+  //check for validation errors and throw them
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+  //saving user to database
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "Not able to add user in DB",
+      });
     }
+    res.json(user);
+  });
+};
 
-    exports.signUp =  (req, res) => {
-        //console.log("wew")
-          const user = new User(req.body)
+//login route
+exports.logIn = (req, res) => {
+  const {email,password} = req.body;
 
-        //check for validation errors and throw them
-        const errors = validationResult(req)
-          if(!errors.isEmpty())
-          {
-            return res.status(422).json({
-              error: errors.array()[0].msg 
-            })
-          }
-          //saving user to database
-          user.save((err,user)=>{
-              if(err)
-              {
-                  return res.status(400).json(
-                      {
-                        err:"Not able to add user in DB"  
-                      }
-                  )
-                  
-              }
-              res.json(user)
-          })
-        }
+  //check for validation errors and throw them
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
+
+  User.findOne({email},(err,user)=>{
+    if(err || !user)
+    {
+      return res.status(400).json({
+        error:"User not found"
+      })
+    }
+    if(!user.authenticate(password)){
+      return res.status(401).json({
+        error:"Email and password do not match"
+      })
+    }
+    // create token
+    const token = jwt.sign({_id:user._id},process.env.HASH)
+
+    // put token in cookie
+    res.cookie("token",token,{expire : new Date()+9999})
+
+    //send resp to front end
+    const {_id,name,email,role} = user;
+    return res.json({token,user:{_id,name,email,role}})
+  })
+};
+
+exports.signOut = (req, res) => {
+  //console.log("wew")
+  res.send("Sign out successful");
+};
--- routes/authentication.js
+++ routes/authentication.js
@@ -4,10 +4,9 @@ const { check,validationResult} = require('express-validator');
 var express = require("express");
 var router = express.Router();
 
-const {signOut,signUp} = require("../controllers/authentication")
+const {signOut,signUp,logIn} = require("../controllers/authentication")
 
 router.get("/signout",signOut);
-
 
 
 router.post("/signup",[
@@ -15,5 +14,12 @@ router.post("/signup",[
     check("email").isEmail().withMessage("email is required"),
     check("password").isLength({min:5}).withMessage("Min lenght 5")
 ],signUp)
+
+router.post("/login",[
+    check("email").isEmail().withMessage("email is required"),
+    check("password").isLength({min:1}).withMessage("Password is required")
+],logIn)
+
+
 
 module.exports = router;
